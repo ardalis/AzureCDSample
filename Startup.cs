94d1c52d6d6655a55ea1199928d0ad861d8ae340
Steve Smith <steve@kentsmiths.com>
2018-08-16 14:28:10 -0400

    Adding default file support

--- Startup.cs
+++ Startup.cs
@@ -11,13 +11,10 @@ namespace AzureCDSample
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -25,6 +22,7 @@ namespace AzureCDSample
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.Run(async (context) =>
