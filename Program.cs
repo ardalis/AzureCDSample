11bbb58e4c4ac3eb11598c675a9cae78a8a91954
Steve Smith <steve@kentsmiths.com>
2018-08-29 12:06:58 -0400

    Using razor pages template and bakery sample

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace AzureCDSample
+namespace razorpagetest
 {
     public class Program
     {
